fdc2bec628b7845fc080f8de834242162b0e3e9b
Rachel Cross <racross1@gmail.com>
2020-10-28 14:11:38 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   i = 0 
-  found_value_index
+  found_value_index = nil
   while i < array.length do
     if array[i] == value_to_find 
       found_value_index = i
